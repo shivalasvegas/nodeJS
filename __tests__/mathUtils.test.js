8af9738ccb902f23b534cf92967a1a6e0e2d3b98
shivalasvegas <shiva.king@academytrainee.com>
2020-09-11 15:26:12 +0100

    SK - Coverage: test reports generated

---
+++ __tests__/mathUtils.test.js
@@ -0,0 +1,26 @@
+const { sum, positive, negative } = require('../mathUtils');
+
+let vals;
+let sum_of_vals;
+let pos_vals;
+let neg_vals;
+
+// Initialises the values passed to each function
+beforeAll(() => {
+    pos_vals = [2, 1, 3];
+    neg_vals = [-2, -1, -1];
+    vals = pos_vals.concat(neg_vals);
+    sum_of_vals = vals.reduce((x, y) => x + y, 0);
+})
+
+test('the sum of vals should be 2', () => {
+    expect(sum(vals)).toBe(sum_of_vals);
+});
+
+test('should get positive values', () => {
+    expect(positive(vals)).toEqual(pos_vals);
+});
+
+test('should get negative values', () => {
+    expect(negative(vals)).toEqual(neg_vals);
+});
