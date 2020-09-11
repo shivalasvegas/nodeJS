6415ba45e61f7f8b987b8e9ef94a185f4ac1bd77
shivalasvegas <shiva.king@academytrainee.com>
2020-09-11 10:33:45 +0100

    SK - functionBasicOperations: created for Jest tests

---
+++ ec_functionBasicOpperations.js
@@ -0,0 +1,6 @@
+const add = (num1, num2) => num1+num2;
+const mul = (num1, num2) => num1*num2;
+const sub = (num1, num2) => num1-num2;
+const div = (num1, num2) => num1/num2;
+
+module.exports = {add, mul, sub, div};
